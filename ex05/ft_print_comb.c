da600540c1358d7e3c18eb589ad7158a0f6501cd
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-21 22:52:05 +0930

    Progressing on ex05

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -5,24 +5,43 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_comb(void)
-{
+void	ft_loop(int len, int[] numbers)	
+{	
 	int	i;
 	int	j;
 	int	k;
-	int	iter;
-	int	len;
-
+	
 	i = 0;
 	j = 1;
 	k = 1;
+	
+	while (i < len - 1)
+	{
+		while (j < len - 1)
+		{
+			while (k < len - 1)
+			{
+				k++
+			}
+			j++
+		}
+		i++
+	}	
+}
+
+void	ft_print_comb(void)
+{
+	int	iter;
+	int	len;
+	int	numbers[10];
+
 	iter = 10;
 	len = sizeof(numbers) / sizeof(int);
 	while (iter < 10)
 	{
 		numbers[iter] = iter;
 	}
-	int	numbers[10];
+	ft_loop(len, numbers);
 }
 
 int	main(void)
