a442f7925869219b13b352ba0c03b8394149d860
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-21 20:19:47 +0930

    Fix ex01, Complete ex02

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -5,20 +5,20 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	int	n;
 
-	n = 97;
-	while (n < 123)
+	n = 122;
+	while (n > 96)
 	{
 		ft_putchar(n);
-		n++;
+		n--;
 	}
 }
 
 int	main(void)
 {
-	ft_print_alphabet();
+	ft_print_reverse_alphabet();
 	return (0);
 }
