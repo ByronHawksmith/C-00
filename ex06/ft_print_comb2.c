958ef762b2dc17f0551366e012d67b71d5e4de38
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-07-23 06:08:08 +0930

    Add commented main() to ft_print_comb2.c,make vairable names consistent

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -7,35 +7,41 @@ void	ft_putchar(char c)
 
 void	ft_put_digit_2(int n)
 {
-	ft_putchar('0' + (n / 10));
-	ft_putchar('0' + (n % 10));
+	ft_putchar('0' + (n / 10)); // / 10 gets lh value
+	ft_putchar('0' + (n % 10)); // % 10 gets rh value
 }
 
-void	ft_put_combination(int left, int right)
+void	ft_put_combination(int left_d, int right_d)
 {
-	ft_put_digit_2(left);
+	ft_put_digit_2(left_d);
 	ft_putchar(' ');
-	ft_put_digit_2(right);
-	if (left != 98 || right != 99)
+	ft_put_digit_2(right_d);
+	if (left_d != 98 || right_d != 99)
 	{
 		write(1, ", ", 2);
 	}
 }
 
-void	ft_print_comb2(void)	
+void	ft_print_comb2(void)
 {
-	int	left_digit;
-	int	right_digit;
+	int	left_d;
+	int	right_d;
 
-	left_digit = 0;
-	while (left_digit <= 98)
+	left_d = 0;
+	while (left_d <= 98)
 	{
-		right_digit = left_digit + 1;
-		while (right_digit <= 99)
+		right_d = left_d + 1;
+		while (right_d <= 99)
 		{
-			ft_put_combination(left_digit, right_digit);
-			right_digit++;
+			ft_put_combination(left_d, right_d);
+			right_d++;
 		}
-		left_digit++;
+		left_d++;
 	}
 }
+
+// int	main()
+// {
+// 	ft_print_comb2();
+// 	return(0);
+// }
