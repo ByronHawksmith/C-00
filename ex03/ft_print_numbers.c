441a6b66b2ecd88540dadfee7b8b63df1aa3c7b9
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-07-23 06:19:10 +0930

    Move comments to appease norminette, ex03

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -5,12 +5,16 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+/*
+'0' == 48, ASCII
+'9' == 57, ASCII
+*/
 void	ft_print_numbers(void)
 {
-	char	n; // could also be int
+	char	n;
 
-	n = '0'; // '0' == 48
-	while (n <= '9') // '9' == 57
+	n = '0';
+	while (n <= '9')
 	{
 		ft_putchar(n);
 		n++;
