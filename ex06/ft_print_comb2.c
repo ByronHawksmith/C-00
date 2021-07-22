625a262d774b58ea0d09e16f85ac80918b770d66
Byron Hawksmith <bhawksmi@F2N038.42adel.org.au>
2021-07-22 11:53:45 +0930

    Progressing on ex06

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)	
+{
+	int	i;
+	int j;
+
+	i = 98;
+	j = 97;
+
+	while (i > 0)
+	{
+		
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}
