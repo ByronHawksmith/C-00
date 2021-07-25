d7403c357dfaa179081742394f8cbe32ed9839fb
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-07-26 01:54:23 +0930

    Start working on comments for ft_purnbr.c

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,5 +1,15 @@
 #include <unistd.h>
 
+/*
+ * The reason we have to specifically
+ * handle -2147483648 is because we are
+ * converting the value of a negative
+ * signed integer to it's absolute value.
+ *
+ * If we do not handle this case we get
+ * --214748364( as output. This is because
+ * we are over
+ */
 void	ft_putnbr(int nb)
 {
 	char c;
