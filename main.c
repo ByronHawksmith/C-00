3614e466993bb6fdbf6acc84ab415b36619d7635
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-07-26 00:49:18 +0930

    Implement ft_putnbr

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ void	ft_print_numbers(void);
 void	ft_is_negative(int n);
 void	ft_print_comb(void);
 void	ft_print_comb2(void);
+void	ft_putnbr(int nb);
 
 // gcc -Wall -Wextra -Werror ex00/ft_putchar.c ex01/ft_print_alphabet.c ex02/ft_print_reverse_alphabet.c ex03/ft_print_numbers.c ex04/ft_is_negative.c ex05/ft_print_comb.c ex06/ft_print_comb2.c main.c
 int	main(void)
@@ -36,6 +37,18 @@ int	main(void)
 	ft_putchar('\n');
 
 	ft_print_comb2();
+	ft_putchar('\n');
+	ft_putchar('\n');
+
+	ft_putnbr(247);
+	ft_putchar('\n');
+	ft_putnbr(4294966196);
+	ft_putchar('\n');
+	ft_putnbr(-1100);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(0);
 	ft_putchar('\n');
 	ft_putchar('\n');
 
