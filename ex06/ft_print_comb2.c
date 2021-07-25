7c3633dc3ce03636a8a488a4afec5d790b13e104
Byron Hawksmith <byronhawksmith@protonmail.com>
2021-07-25 22:00:36 +0930

    Rename functions in ft_print_comb2.c

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -10,17 +10,17 @@ Assuming a two digit number
 n / 10 gives left hand digit
 n % 10 gives right hand digit
 */
-void	ft_put_digit_2(int n)
+void	print_num(int n)
 {
 	ft_putchar7('0' + (n / 10));
 	ft_putchar7('0' + (n % 10));
 }
 
-void	ft_put_combination(int l_num, int r_num)
+void	print_comb(int l_num, int r_num)
 {
-	ft_put_digit_2(l_num);
+	print_num(l_num);
 	ft_putchar7(' ');
-	ft_put_digit_2(r_num);
+	print_num(r_num);
 	if (l_num != 98 || r_num != 99)
 	{
 		write(1, ", ", 2);
@@ -38,7 +38,7 @@ void	ft_print_comb2(void)
 		r_num = l_num + 1;
 		while (r_num <= 99)
 		{
-			ft_put_combination(l_num, r_num);
+			print_comb(l_num, r_num);
 			r_num++;
 		}
 		l_num++;
